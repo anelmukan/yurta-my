ed535b911039ce423f81b7a2afae07116f8dc015
IndiraTilla <inditilla@gmail.com>
2024-07-17 19:15:48 +0600

    Updates

---
+++ precache-manifest.1463a028b89afffa06777f55f2eaebfe.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5e3bca5a8053b8ce2e1411e9fa162e8",
+    "url": "/yurta-my/index.html"
+  },
+  {
+    "revision": "cefabb63d9470abe3212",
+    "url": "/yurta-my/static/css/2.2ae677b1.chunk.css"
+  },
+  {
+    "revision": "5cf0b2e9446d5d35ad00",
+    "url": "/yurta-my/static/css/main.8d019ff6.chunk.css"
+  },
+  {
+    "revision": "cefabb63d9470abe3212",
+    "url": "/yurta-my/static/js/2.92e4bb1e.chunk.js"
+  },
+  {
+    "revision": "3232a3baa9a05f3e2b89",
+    "url": "/yurta-my/static/js/3.9973b74a.chunk.js"
+  },
+  {
+    "revision": "5cf0b2e9446d5d35ad00",
+    "url": "/yurta-my/static/js/main.2e4c333b.chunk.js"
+  },
+  {
+    "revision": "0e49c1fd5e9f31dfb839",
+    "url": "/yurta-my/static/js/runtime~main.d627d9ad.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/yurta-my/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/yurta-my/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/yurta-my/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/yurta-my/static/media/slick.f97e3bbf.svg"
+  }
+]);
